d95c77a12c8fd32d039d1fc676a0db1220880a1c
devhoody <rethnoly@gmail.com>
2023-05-17 17:43:29 +0900

    반복문(while,for)을 이용한 6, 7

--- Gugudan/src/Gugudan.java
+++ Gugudan/src/Gugudan.java
@@ -2,44 +2,26 @@
 public class Gugudan {
 
 	public static void main(String[] args) {
-		int i = 4 * 1;
-		System.out.println(i);
-		i = 4 * 2;
-		System.out.println(i);		
-		i = 4 * 3;
-		System.out.println(i);		
-		i = 4 * 4;
-		System.out.println(i);		
-		i = 4 * 5;
-		System.out.println(i);		
-		i = 4 * 6;
-		System.out.println(i);		
-		i = 4 * 7;
-		System.out.println(i);		
-		i = 4 * 8;
-		System.out.println(i);		
-		i = 4 * 9;
-		System.out.println(i);		
-		
-		
-		i = 5 * 1;
-		System.out.println(i);		
-		i = 5 * 2;
-		System.out.println(i);		
-		i = 5 * 3;
-		System.out.println(i);		
-		i = 5 * 4;
-		System.out.println(i);		
-		i = 5 * 5;
-		System.out.println(i);		
-		i = 5 * 6;
-		System.out.println(i);		
-		i = 5 * 7;
-		System.out.println(i);		
-		i = 5 * 8;
-		System.out.println(i);		
-		i = 5 * 9;
-		System.out.println(i);		
+
+		int i = 1;
+
+		while (i < 10) {
+			System.out.println(6 * i);
+			i++;
+		}
+
+		for (i = 1; i < 10; i++) {
+			System.out.println(6 * i);
+		}
+
+		while (i < 10) {
+			System.out.println(7 * i);
+			i++;
+		}
+
+		for (i = 1; i < 10; i++) {
+			System.out.println(7 * i);
+		}
 	}
 
 }
