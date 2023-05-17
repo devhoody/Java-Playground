3ee27c193406ad5477384c07e183afa57fd301bc
devhoody <rethnoly@gmail.com>
2023-05-17 18:00:53 +0900

    메소드를 활용한 구구단 구현
    calculate(), print()

--- Gugudan/src/Gugudan.java
+++ Gugudan/src/Gugudan.java
@@ -1,15 +1,28 @@
 
 public class Gugudan {
 
-	public static void main(String[] args) {
+	public static int[] calculate(int number) {
 		int[] result = new int[9];
 
 		for (int i = 0; i < result.length; i++) {
-			result[i] = 2 * (i + 1);
+			result[i] = number * (i + 1);
 		}
 
+		return result;
+	}
+
+	public static void print(int[] result) {
 		for (int i = 0; i < result.length; i++) {
 			System.out.println(result[i]);
 		}
+
 	}
+
+	public static void main(String[] args) {
+		for (int i = 2; i < 10; i++) {
+			print(calculate(i));
+
+		}
+	}
+
 }
