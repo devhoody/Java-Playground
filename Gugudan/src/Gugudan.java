3f5bea2ed131365d7e9557288fa69d3b0bd3a49a
devhoody <rethnoly@gmail.com>
2023-05-17 17:38:16 +0900

    변수 i를 이용한 4,5단

--- Gugudan/src/Gugudan.java
+++ Gugudan/src/Gugudan.java
@@ -2,25 +2,44 @@
 public class Gugudan {
 
 	public static void main(String[] args) {
-		System.out.println(2*1);
-		System.out.println(2*2);
-		System.out.println(2*3);
-		System.out.println(2*4);
-		System.out.println(2*5);
-		System.out.println(2*6);
-		System.out.println(2*7);
-		System.out.println(2*8);
-		System.out.println(2*9);
+		int i = 4 * 1;
+		System.out.println(i);
+		i = 4 * 2;
+		System.out.println(i);		
+		i = 4 * 3;
+		System.out.println(i);		
+		i = 4 * 4;
+		System.out.println(i);		
+		i = 4 * 5;
+		System.out.println(i);		
+		i = 4 * 6;
+		System.out.println(i);		
+		i = 4 * 7;
+		System.out.println(i);		
+		i = 4 * 8;
+		System.out.println(i);		
+		i = 4 * 9;
+		System.out.println(i);		
 		
-		System.out.println(3*1);
-		System.out.println(3*2);
-		System.out.println(3*3);
-		System.out.println(3*4);
-		System.out.println(3*5);
-		System.out.println(3*6);
-		System.out.println(3*7);
-		System.out.println(3*8);
-		System.out.println(3*9);
+		
+		i = 5 * 1;
+		System.out.println(i);		
+		i = 5 * 2;
+		System.out.println(i);		
+		i = 5 * 3;
+		System.out.println(i);		
+		i = 5 * 4;
+		System.out.println(i);		
+		i = 5 * 5;
+		System.out.println(i);		
+		i = 5 * 6;
+		System.out.println(i);		
+		i = 5 * 7;
+		System.out.println(i);		
+		i = 5 * 8;
+		System.out.println(i);		
+		i = 5 * 9;
+		System.out.println(i);		
 	}
 
 }
