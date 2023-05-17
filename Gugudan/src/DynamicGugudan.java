9706e562b71cb3293861278afe3e94c3206f8965
devhoody <rethnoly@gmail.com>
2023-05-17 18:13:10 +0900

    (배열이용)입력한 값에 따른 크기가 다른 구구단 구현

--- Gugudan/src/DynamicGugudan.java
+++ Gugudan/src/DynamicGugudan.java
@@ -7,10 +7,12 @@ public class DynamicGugudan {
 
 		System.out.println("원하는 단을 입력하세요.");
 
-		int number = sc.nextInt();
+		int N = sc.nextInt();
+		
+		int[] arr = new int[N];
 
-		for (int i = 2; i <= number; i++) {
-			for (int j = 1; j <= number; j++) {
+		for (int i = 2; i <= arr.length; i++) {
+			for (int j = 1; j <= arr.length; j++) {
 				System.out.printf("%d * %d = %d\n", i, j, i * j);
 			}
 		}
