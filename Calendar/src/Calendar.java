dcadbe73e61f92e0ac0a40582fa115e4587ae27f
devhoody <rethnoly@gmail.com>
2023-05-18 16:54:28 +0900

    instance 변수를 이용한 해당월 최대일수 구하기

--- Calendar/src/Calendar.java
+++ Calendar/src/Calendar.java
@@ -2,29 +2,30 @@ import java.util.Scanner;
 
 public class Calendar {
 
-	private final int[] Max_Days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 31, 31 };
+	private static final int[] Max_Days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 31, 31 };
 
-	public  int MaxDaysOfMonth(int month) {
-		return Max_Days[month-1];
+	public int getMaxDaysOfMonth(int month) {
+		return Max_Days[month - 1];
+	}
+	
+	public void printSampleCalendar() {
+		System.out.println("일 월 화 수 목 금 토");
+		System.out.println("--------------------");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		Calendar calendar = new Calendar();
-		
+		Calendar cal = new Calendar();
+
 		System.out.println("달을 입력하세요.");
 		int month = sc.nextInt();
-		
-		System.out.println(calendar.MaxDaysOfMonth(month));
+
+		System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxDaysOfMonth(month));
 		sc.close();
-
-//		System.out.println("일 월 화 수 목 금 토");
-//		System.out.println("--------------------");
-//		System.out.println(" 1  2  3  4  5  6  7");
-//		System.out.println(" 8  9 10 11 12 13 14");
-//		System.out.println("15 16 17 18 19 20 21");
-//		System.out.println("22 23 24 25 26 27 28");
-
 	}
 
 }
