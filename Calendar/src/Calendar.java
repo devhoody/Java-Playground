9ad481f703c3569ed1356824550c18f94fac2070
devhoody <rethnoly@gmail.com>
2023-05-22 15:19:39 +0900

    update 윤년 추가

--- Calendar/src/Calendar.java
+++ Calendar/src/Calendar.java
@@ -3,8 +3,19 @@ import java.util.Scanner;
 public class Calendar {
 
 	private static final int[] Max_Days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LEAP_Max_Days = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	public int getMaxDaysOfMonth(int month) {
+	public boolean isLeapYear(int year) {
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+			return true;
+		else
+			return false;
+	}
+
+	public int getMaxDaysOfMonth(int year, int month) {
+		if (isLeapYear(year)) {
+			return LEAP_Max_Days[month - 1];
+		}
 		return Max_Days[month - 1];
 	}
 
@@ -12,8 +23,9 @@ public class Calendar {
 		System.out.printf("   <<%4d%3d¿ù>> \n", year, month);
 		System.out.println(" SU MO TY WE TH FR SA");
 		System.out.println("--------------------");
-		
-		int maxDay = getMaxDaysOfMonth(month);
+
+		int maxDay = getMaxDaysOfMonth(year, month);
+
 		for (int i = 1; i <= maxDay; i++) {
 			System.out.printf("%3d", i);
 			if (i % 7 == 0) {
@@ -21,6 +33,7 @@ public class Calendar {
 			}
 		}
 		System.out.println();
+
 //		System.out.println(" 1  2  3  4  5  6  7");
 //		System.out.println(" 8  9 10 11 12 13 14");
 //		System.out.println("15 16 17 18 19 20 21");
--- Calendar/src/Prompt.java
+++ Calendar/src/Prompt.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 
 public class Prompt {
 
-	private final static String PROMPT = "cal>";
-
 	public void runPrompt() {
 		Scanner sc = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
 		while (true) {
+			System.out.println("년도를 입력하세요.");
+			System.out.println("YEAR > ");
+			int year = sc.nextInt();
 			System.out.println("월을 입력하세요.");
-			System.out.println(PROMPT);
+			System.out.println("MONTH > ");
 			int month = sc.nextInt();
 			if (month == -1) {
 				break;
@@ -18,7 +19,7 @@ public class Prompt {
 			if (month > 12) {
 				continue;
 			}
-			cal.printSampleCalendar(2023, month);
+			cal.printSampleCalendar(year, month);
 //			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
 		}
 		System.out.println("Have a nice Day!");
